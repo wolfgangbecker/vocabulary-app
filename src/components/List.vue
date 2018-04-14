eabaf84309c7670a2c54146dc3560cf7d0013dc9
Wolfgang Becker <wolli.becker@gmail.com>
2018-04-14 17:30:00 +0200

    Initial commit

---
+++ src/components/List.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Vocabulary App</h1>
+    <form class="form">
+      <label for="word">Word</label>
+      <input type="text" id="word">
+      <label for="translation">Translation</label>
+      <input type="text" id="translation">
+    </form>
+
+    <div class="list"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
